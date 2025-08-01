f8bcdd4c5c1035ef74107e0fc05327edadeadc79
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-08-02 01:29:38 +0530

    #141 Linked List Cycle - Easy

---
+++ 141_easy.cpp
@@ -0,0 +1,17 @@
+bool hasCycle(ListNode *head) {
+
+    if (!head || !(head->next))
+        return false;
+
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (fast && fast->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
+            return true;
+    }
+
+    return false;
+}

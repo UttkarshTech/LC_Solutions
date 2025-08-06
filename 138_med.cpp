9344701216298073949b80b60b3373e79e21f511
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-08-07 01:40:07 +0530

    #138 Copy List with Random Pointer - Medium

--- 138_med.cpp
+++ 138_med.cpp
@@ -3,6 +3,46 @@ Node* copyRandomList(Node* head) {
         return head;
     if (!head->next){
         Node *newNode = new Node(head->val);
+        if (head->random == head)
+            newNode->random = newNode;
+        return newNode;
+    }
+    Node *run = head;
+    while (run){
+        Node *newNode = new Node(run->val);
+        newNode->next = run->next;
+        run->next = newNode;
+        run = run->next->next;
+    }
+    run = head;
+    while (run){
+        if (!run->random)
+            run->next->random = NULL;
+        else
+            run->next->random = run->random->next;
+        run = run->next->next;
+    }
+    Node *newHead = head->next;
+    run = head;
+    while (run){
+        if (run->next && run->next->next){
+            Node *temp = run->next->next;
+            run->next->next = run->next->next->next;
+            run->next = temp;
+            run = temp;
+        } else{
+            run->next = NULL;
+            run = NULL;
+        }
+    }
+    return newHead;
+}
+
+Node* copyRandomList(Node* head) {
+    if (!head)
+        return head;
+    if (!head->next){
+        Node *newNode = new Node(head->val);
         if (head->random)
         newNode->random = newNode;
         return newNode;

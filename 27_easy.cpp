7c0ba8db8767aa781af878db5c526e9dbbdc7200
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-08-09 18:49:43 +0530

    #27 Remove Elements - Easy

---
+++ 27_easy.cpp
@@ -0,0 +1,8 @@
+int removeElement(vector<int>& nums, int val) {
+        int pos = 0;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] != val)
+                nums[pos++] = nums[i];
+        }
+        return pos;
+    }

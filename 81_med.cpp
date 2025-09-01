44545fafd0f4e603facfebf1e49965963ff598f8
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-09-01 23:28:47 +0530

    #81 Search in Rotated Sorted Array II - Medium

---
+++ 81_med.cpp
@@ -0,0 +1,25 @@
+bool search(vector<int>& nums, int target) {
+    int low = 0, high = nums.size()-1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (nums[mid] == target || nums[low] == target || nums[high] == target)
+            return true;
+        if (nums[low] == nums[mid] && nums[mid] == nums[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if (nums[low] <= nums[mid]){
+            if (target < nums[mid] && target > nums[low])
+                high = mid - 1;
+            else low = mid + 1;
+
+        } else {
+            if (target > nums[mid] && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return false;
+}

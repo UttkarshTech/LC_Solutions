66ab2eafb5b6386bf3364ab96a3086ec376a2576
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-09-24 14:00:44 +0530

    #378 Kth Smallest Element in a Sorted Matrix - Medium

--- 378_medium.cpp
+++ 378_medium.cpp
@@ -1,11 +1,31 @@
 int kthSmallest(vector<vector<int>>& matrix, int k) {
-    priority_queue<int> pq;
-    for (auto row : matrix){
-        for (auto element : row){
-            pq.push(element);
-            if (pq.size() > k)
-                pq.pop();
+    int size = matrix.size();
+    int l = matrix[0][0], h = matrix[size-1][size-1], mid;
+    while(l <= h){
+        mid = (l + h) / 2;
+        int count = 0, j = size - 1;
+        for(int i=0; i<size; i++){
+            while(j>=0 && matrix[i][j]>mid)
+                j--;
+            count += j+1;
         }
+        if(count < k)
+            l = mid+1;
+        else
+            h = mid-1;
     }
-    return pq.top();
+    return l;
 }
+
+
+// int kthSmallest(vector<vector<int>>& matrix, int k) {
+//     priority_queue<int> pq;
+//     for (auto row : matrix){
+//         for (auto element : row){
+//             pq.push(element);
+//             if (pq.size() > k)
+//                 pq.pop();
+//         }
+//     }
+//     return pq.top();
+// }
